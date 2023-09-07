8445f8b4e1203303f8045d42e8aad0c4ca9b5dfe
Brijesh Sharma <brijesh.sharma@echttech.com>
2023-09-07 09:47:39 +0530

    file path

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,6 +43,11 @@ return [
             'visibility' => 'public',
             'throw' => false,
         ],
+        
+        'resources_views' => [
+            'driver' => 'local',
+            'root' => base_path('resources/uploads'),
+        ],
 
         's3' => [
             'driver' => 's3',
