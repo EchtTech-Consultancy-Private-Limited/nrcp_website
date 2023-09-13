80955cecf267936a8a2d65cfc989da9c2c2b850a
Brijesh Sharma <brijesh.sharma@echttech.com>
2023-09-13 15:07:43 +0530

    bug fix

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php 
+
+return[
+
+    'brand' => [
+        'default_banner' => env('DEFAULT_BANNER_URL', "assets/Nrcp_img/banner/banner-m.JPG"),
+        'default_logo' => env('DEFAULT_LOGO', "assets/Nrcp_img/logo-gov.png"),
+    ],
+
+];
