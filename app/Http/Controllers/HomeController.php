53845cd31cf716a1f90fc4d99cb754a5cdea41d2
your_username <vishal.singh@echttech.com>
2023-09-22 17:45:23 +0530

    NRCP Video and gallery page changes also add Facebook slider

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,6 +36,11 @@ class HomeController extends Controller
     {
         return view('human-health'); 
     }
+
+    public function videoDetail()
+    {
+        return view('videos'); 
+    }
     
     public function mediaalerts()
     {
