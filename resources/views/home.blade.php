4730e8182fabd5fe085ad69fab33135f5652b09b
your_username <vishal.singh@echttech.com>
2023-09-25 19:12:59 +0530

    latest news text change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -82,7 +82,7 @@
                 <div class="row">
                     <div class="col-lg-2 col-md-2">
                         <div class="latest-news-text">
-                            <p class="m-0">Latest News</p>
+                            <p class="m-0">Recent Activity</p>
                         </div>
                     </div>
                     <div class="col-lg-9 col-md-9">
