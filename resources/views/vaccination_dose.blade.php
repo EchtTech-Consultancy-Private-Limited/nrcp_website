7a47ccccde45b88cc540db3bbb239205541b91f6
ET-112 <monu.tyagi@echttech.com>
2023-12-04 14:02:30 +0530

    state and dirtrict name show

--- resources/views/vaccination_dose.blade.php
+++ resources/views/vaccination_dose.blade.php
@@ -554,6 +554,7 @@
                                                                                 <div class="search-widget mb-20">
                                                                                     <div class="row">
                                                                                         <div class="col-lg-5">
+                                                                                            
                                                                                             <select name="state"
                                                                                                 id="stateA"
                                                                                                 class="form-control"
@@ -581,7 +582,7 @@
                                                                                                 </select>
                                                                                             </div>
                                                                                         </div>
-
+                                                                                        
                                                                                         <div class="col-lg-2">
                                                                                             <div
                                                                                                 class="btn-part view-all-btn ">
@@ -620,7 +621,9 @@
                                                                             {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15281445.36555606!2d72.11145795589131!3d20.758022812161478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1694066812387!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" width="100%" height="300px"></iframe> --}}
 
                                                                             <div id="newMapA"></div>
+                                                                          
                                                                         </div>
+                                                                
                                                                     </div>
 
                                                                 </div>
